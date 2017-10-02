c55a5c63edaa7111e0ac4cfab1d1fc413694a6d5
Elisa Vaccaro <erv230@nyu.edu>
2017-10-02 19:12:52 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(list)
+  answer = []
+  list.each do |name|
+    answer.push(badge_maker(name))
+  end
+  answer
+end
+
+def assign_rooms(list)
+  assignment = []
+  list.each do |name|
+    assignment.push("Hello, #{name}! You'll be assigned to room #{list.index(name)+1}!")
+  end
+  assignment
+end
+
+def printer
+  puts batch_badge_creator
+  puts assign_rooms
+end
